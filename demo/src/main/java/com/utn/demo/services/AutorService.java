8431c48daba0ed0c1a0a991579d7f2d6ddc627e0
EmilioMena48 <142940780+EmilioMena48@users.noreply.github.com>
2023-10-03 20:16:04 -0300

    Delete demo directory

--- demo/src/main/java/com/utn/demo/services/AutorService.java
+++
@@ -1,7 +0,0 @@
-package com.utn.demo.services;
-
-import com.utn.demo.entities.Autor;
-import com.utn.demo.entities.Base;
-
-public interface AutorService  extends BaseService<Autor, Long> {
-}
--- demo/src/main/java/com/utn/demo/services/PersonaService.java
+++
@@ -1,6 +0,0 @@
-package com.utn.demo.services;
-
-import com.utn.demo.entities.Persona;
-
-public interface PersonaService extends BaseService<Persona, Long>{
-}
