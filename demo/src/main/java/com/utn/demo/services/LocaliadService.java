167addd8c200a57d29621f45f344177d9ecd07b2
EmilioMena48 <142940780+EmilioMena48@users.noreply.github.com>
2023-10-03 20:52:40 -0300

    Delete demo directory

--- demo/src/main/java/com/utn/demo/services/LocaliadService.java
+++
@@ -1,7 +0,0 @@
-package com.utn.demo.services;
-
-import com.utn.demo.entities.Autor;
-import com.utn.demo.entities.Localidad;
-
-public interface LocaliadService extends BaseService<Localidad, Long> {
-}
